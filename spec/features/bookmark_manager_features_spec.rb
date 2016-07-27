34e14902602a2fc449f02d2e3de9b872c9488dec
albiebabie <albiebabie@gmail.com>
2016-07-27 13:09:30 +0100

    challenge 16 - filtering tags

--- spec/features/bookmark_manager_features_spec.rb
+++ spec/features/bookmark_manager_features_spec.rb
@@ -10,4 +10,13 @@ feature '#viewing_links' do
       expect(page).to have_content 'google'
     end
   end
+  scenario 'user can view links by tags' do
+    Link.create(title: 'bbc', url: 'www.bbc.co.uk', tags: [Tag.first_or_create(name: 'news')])
+    Link.create(title: 'bubbles', url: 'www.bubbles.co.uk', tags: [Tag.first_or_create(name: 'bubbles')])
+    visit '/tags/bubbles'
+    within 'ul#links' do
+      expect(page).to have_content('bubbles')
+      expect(page).not_to have_content('bbc')
+    end
+  end
 end
