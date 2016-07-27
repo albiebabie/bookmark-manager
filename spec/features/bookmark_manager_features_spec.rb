d50ec5bf2cf9e4df1bcbd26cf06070b3097961c1
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-27 15:34:33 +0100

    add multiple tags

--- spec/features/bookmark_manager_features_spec.rb
+++ spec/features/bookmark_manager_features_spec.rb
@@ -2,15 +2,14 @@
 feature '#viewing_links' do
   scenario 'user views link on homepage' do
     Link.create(title: 'google', url: 'www.google.com' )
-
     visit '/links'
     expect(page.status_code).to eq 200
-
     within('ul#links') do
-      expect(page).to have_content 'google'
+    expect(page).to have_content 'google'
     end
   end
-  scenario 'user can view links by tags' do
+
+  scenario 'user can view links by the tag' do
     Link.create(title: 'bbc', url: 'www.bbc.co.uk', tags: [Tag.first_or_create(name: 'news')])
     Link.create(title: 'bubbles', url: 'www.bubbles.co.uk', tags: [Tag.first_or_create(name: 'bubbles')])
     visit '/tags/bubbles'
--- spec/features/creating_tags_spec.rb
+++ spec/features/creating_tags_spec.rb
@@ -4,9 +4,19 @@ feature '#tags' do
     click_link('new_link')
     fill_in :url, with: 'bbc.com'
     fill_in :title, with: 'bbc'
-    fill_in :tag, with: 'news'
+    fill_in :tags, with: 'news'
     click_button('submit')
     link = Link.first
     expect(link.tags.map(&:name)).to include('news')
+  end
+  scenario 'user can add multiple tags' do
+    visit '/links'
+    click_link('new_link')
+    fill_in :url, with: 'bbc.com'
+    fill_in :title, with: 'bbc'
+    fill_in :tags, with: 'news media'
+    click_button('submit')
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('news', 'media')
   end
 end
