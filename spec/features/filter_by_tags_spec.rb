46fb7ba5417db9d9f554e7845754575a221ba7d1
acsauk <acsauk@gmail.com>
2016-07-27 14:13:40 +0100

    completes exercise #16 - filtering by links

---
+++ spec/features/filter_by_tags_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper.rb'
+
+feature 'filter tags' do
+  before do
+    Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search engine')] )
+    Link.create(url: 'http://www.generalassembly.com', title: 'General Assembly', tags: [Tag.first_or_create(name: 'education')] )
+    Link.create(url: 'http://www.bubblerus.com', title: 'Bubbles R Us', tags: [Tag.first_or_create(name: 'bubbles')] )
+    Link.create(url: 'http://www.bubblerus.co.uk', title: 'Bubbles R Us - UK', tags: [Tag.first_or_create(name: 'bubbles')] )
+  end
+
+  scenario 'to be able to filter links by tags' do
+    visit '/tags/bubbles'
+
+    expect(page.status_code).to eq(200)
+
+    within 'ul#links' do
+      expect(page).to have_content('Bubbles R Us')
+      expect(page).to have_content('Bubbles R Us - UK')
+      expect(page).not_to have_content('Google')
+      expect(page).not_to have_content('General Assembly')
+    end
+  end
+end
