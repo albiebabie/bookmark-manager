8a0cb44547c5bb22b9e07c59423e06faa1b6a23a
albiebabie <albiebabie@gmail.com>
2016-07-28 12:00:42 +0100

    challenge 18 - done!

--- app/app.rb
+++ app/app.rb
@@ -42,6 +42,12 @@ set :session_secret, 'super secret'
     redirect to('/links')
   end
 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
